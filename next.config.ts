b49bf285da18c5b773cafa5ee63c7266cadfbb6d
Sepsu Dev <sepsu.dev@gmail.com>
2026-09-09 16:46:04 +0700

    Remove image optimization settings from config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.r2.dev",
-      },
-      {
-        protocol: "https",
-        hostname: "pub-0a25e325405640159e496897aa2711a5.r2.dev",
-      },
-    ],
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
